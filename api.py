4dd2a73c541169786b8456306ca300efaf15a2c6
Wang chi-chien <kennywang2003@gmail.com>
2024-02-07 16:25:46 +0800

    predict

--- api.py
+++ api.py
@@ -3,11 +3,12 @@ from flask_cors import CORS
 
 from okx.api import Market as Market_api
 from okx.app import OkxSPOT
-import pandas as pd
-from datetime import datetime
-from dotenv import load_dotenv
 
-load_dotenv()
+import pandas as pd
+import time
+import statsmodels.api as sm
+from datetime import datetime
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume', 'volCcy', 'volCcyQuote', 'confirm']
 date_string_after = ['2023-1-31','2023-2-28','2023-3-31','2023-4-30','2023-5-31','2023-6-30',
@@ -53,17 +54,94 @@ def History_finder(y, m):
 
     return full_data
 
+def arima_AIC(data, p = 4, d = 4, q = 4):
+    
+    start_time = time.time()
+    
+    # MSE
+    period = 1
+    L = len(data)
+    train = data[ : (L - period)]
+    test = data[ - period: ]
+    mse_r = []
+    # AIC
+    best_pdq =["AIC_pdq", 10000, "MSE", 10000]
+    AIC = []
+    
+    for i in range(p): # AR
+        
+        for j in range(1, d): # I
+            
+            for k in range(q): # MA
+                
+                model = sm.tsa.arima.ARIMA(data, order = (i,j,k))
+                fitted = model.fit()
+                # MSE
+                forecast = fitted.forecast(step = period, alpha = 0.05)
+                mse = mean_squared_error(test, forecast)
+                mse_r.append(mse)
+                # AIC
+                AIC.append(fitted.aic)
+                
+                if fitted.aic < best_pdq[1]:
+                    
+                    best_pdq[0] = (i, j, k)
+                    best_pdq[1] = fitted.aic
+                    
+                if mse < best_pdq[1]:
+                    
+                    best_pdq[2] = (i, j, k)
+                    best_pdq[3] = mse
+                    
+    end_time = time.time()
+    print(f"used time : {end_time - start_time}")
+    
+    return best_pdq[0], best_pdq[2]
+
+def predict(full_data, best_pdq_MSE):
+
+    model = sm.tsa.ARIMA(full_data['open'], order = best_pdq_MSE)
+    fitted = model.fit()
+    number_of_steps = 10
+    forecast = fitted.forecast(steps = number_of_steps)
+
+    ten_days_after = forecast.values[9]
+    one_days_after = forecast.values[0]
+
+    percentage_change = ((ten_days_after - one_days_after) / one_days_after) * 100
+
+    return forecast, percentage_change
+
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def home():
-    # for m in range(8, 13):
-    #     full_data = History_finder(2023, m)
+
+    for m in range(8, 13):
+        full_data = History_finder(2023, m)
 
     full_data = History_finder(2024, 1)
 
-    return full_data.to_json(orient = 'records')
+    best_pdq_AIC, best_pdq_MSE = arima_AIC(full_data['open'], 4, 4, 4)
+    print(best_pdq_MSE)
+
+    forecast, percentage_change = predict(full_data, best_pdq_MSE)
+
+    # 整合成 JSON 物件
+    result = {
+        "full_data": full_data.to_json(orient = 'records'),
+        "error": {
+            "best_pdq_AIC": best_pdq_AIC,
+            "best_pdq_MSE": best_pdq_MSE
+        },
+        "predict": {
+            "forecast": forecast.to_json(orient = 'records'),
+            "percentage_change": percentage_change
+        }
+    }
+
+    return result
 
 if __name__ == '__main__':
     app.run(debug = True)
--- src/app/page.js
+++ src/app/page.js
@@ -10,91 +10,106 @@ export default function Home() {
   // let chartRef;
   const [selectedOption, setSelectedOption] = useState('');
   const [apiData, setApiData] = useState(null);
+  const [errorData, setErrorData] = useState(null);
+  const [predictData, setPredictData] = useState(null);
+
+  const [chartRef1, setChartRef1] = useState(null);
+  const [chartRef2, setChartRef2] = useState(null);
 
   const handleSelectChange = (event) => {
     setSelectedOption(event.target.value);
   };
 
   useEffect(() => {
+
     const fetchData = async () => {
+
       try {
-        // if (selectedOption) {
-          console.log('fetching');
-          // Use a relative path instead of an absolute URL
-          const response = await axios.get("http://127.0.0.1:5000/");
-  
-          setApiData(response.data);
-          const stockData = response.data;
-          // get dates and prices
-          const dates = stockData.map(entry => new Date(entry.date).toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' }));
-          const prices = stockData.map(entry => entry.close);
-          // Chart.js
-          const ctx = document.getElementById('stockChart').getContext('2d');
-          const ctx2 = document.getElementById('stockChart2').getContext('2d');
-          const chartRef = new Chart(ctx, {
-              type: 'line',
-              data: {
-                  labels: dates,
-                  datasets: [{
-                      label: '股價',
-                      borderColor: 'rgb(75, 192, 192)',
-                      data: prices,
-                  }],
-              },
-              options: {
-                  scales: {
-                      x: [{
-                          type: 'time',
-                          time: {
-                              unit: 'day',  // 根據你的數據間隔調整
-                          },
-                          title: {
-                              display: true,
-                              text: '日期',
-                          },
-                      }],
-                      y: {
-                          title: {
-                              display: true,
-                              text: '股價',
-                          },
-                      },
-                  },
-              },
-          });
-          console.log(chartRef);
-          const chartRef2 = new Chart(ctx2, {
-              type: 'line',
-              data: {
-                  labels: dates,
-                  datasets: [{
-                      label: '股價',
-                      borderColor: 'rgb(75, 192, 192)',
-                      data: prices,
-                  }],
-              },
-              options: {
-                  scales: {
-                      x: [{
-                          type: 'time',
-                          time: {
-                              unit: 'day',  // 根據你的數據間隔調整
-                          },
-                          title: {
-                              display: true,
-                              text: '日期',
-                          },
-                      }],
-                      y: {
-                          title: {
-                              display: true,
-                              text: '股價',
-                          },
-                      },
-                  },
-              },
-          });
-        // }
+        // Use a relative path instead of an absolute URL
+        const response = await axios.get("http://127.0.0.1:5000/");
+        setApiData(JSON.parse(response.data.full_data));
+        setErrorData(response.data.error);
+        setPredictData([JSON.parse(response.data.predict.forecast), response.data.predict.percentage_change]);
+        const stockData = JSON.parse(response.data.full_data);
+        // get dates and prices
+        const dates = stockData.map(entry => new Date(entry.date).toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' }));
+        const prices = stockData.map(entry => entry.close);
+        // Chart.js
+        const ctx = document.getElementById('stockChart').getContext('2d');
+        const ctx2 = document.getElementById('stockChart2').getContext('2d');
+
+        // Destroy previous chart instances
+        if (chartRef1) {
+          chartRef1.destroy();
+        }
+        if (chartRef2) {
+          chartRef2.destroy();
+        }
+        const newChartRef1 = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: dates,
+                datasets: [{
+                    label: '股價',
+                    borderColor: 'rgb(75, 192, 192)',
+                    data: prices,
+                }],
+            },
+            options: {
+                scales: {
+                    x: [{
+                        type: 'time',
+                        time: {
+                            unit: 'day',  // 根據你的數據間隔調整
+                        },
+                        title: {
+                            display: true,
+                            text: '日期',
+                        },
+                    }],
+                    y: {
+                        title: {
+                            display: true,
+                            text: '股價',
+                        },
+                    },
+                },
+            },
+        });
+        const newChartRef2 = new Chart(ctx2, {
+            type: 'line',
+            data: {
+                labels: dates,
+                datasets: [{
+                    label: '股價',
+                    borderColor: 'rgb(75, 192, 192)',
+                    data: prices,
+                }],
+            },
+            options: {
+                scales: {
+                    x: [{
+                        type: 'time',
+                        time: {
+                            unit: 'day',  // 根據你的數據間隔調整
+                        },
+                        title: {
+                            display: true,
+                            text: '日期',
+                        },
+                    }],
+                    y: {
+                        title: {
+                            display: true,
+                            text: '股價',
+                        },
+                    },
+                },
+            },
+        });
+        setChartRef1(newChartRef1);
+        setChartRef2(newChartRef2);
+
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -103,13 +118,20 @@ export default function Home() {
     fetchData();
   }, [selectedOption]);
 
+  useEffect(() => {
+    console.log(errorData);
+  }, [errorData]);
+  useEffect(() => {
+    console.log(predictData);
+  }, [predictData]);
+
   return (
     <main className="flex flex-col items-center p-8 h-screen">
       <section className="flex flex-col items-center justify-center p-8 w-full">
         {/* choose model */}
-        <div className="mt-8">
+        <div className="mt-8 flex justify-start">
           <label htmlFor="selectOption" className="text-2xl font-semibold mb-2">
-            Model:
+            StoneTrader
           </label>
           <select
             id="selectOption"
@@ -117,96 +139,132 @@ export default function Home() {
             onChange={handleSelectChange}
             className="p-2 border border-gray-300 rounded-md"
           >
-            <option value="">choose</option>
+            <option value="">model</option>
             <option value="option1">ARIMA</option>
             <option value="option2">Profit</option>
           </select>
-  
-          {selectedOption && (
-            <p className="mt-2">selected:{selectedOption}</p>
-          )}
         </div>
         <div className="my-8"></div>
         {/* pred */}
         <div className="flex justify-between w-full">
           <div className="w-50%">
             {/* chart */}
-            predict chart
-            <canvas id="stockChart" width="500" height="400"></canvas>
+            10 days avg chart
+            <canvas id="stockChart" width="600" height="400"></canvas>
           </div>
           <div className="w-50%">
             {/* chart */}
-            predict chart
-            <canvas id="stockChart2" width="500" height="400"></canvas>
+            1 week avg chart
+            <canvas id="stockChart2" width="600" height="400"></canvas>
           </div>
         </div>
         <div className="my-8"></div>
         {/* history and statistics */}
         <div className="flex justify-between w-full">
-          <div className="w-30%">
-              {apiData && apiData.length > 0 ? (
-              <div className = "mt-8 overflow-y-auto" style = {{ maxHeight: '200px' }}>
-                <h2>Data from API:</h2>
-                <table>
-                  <thead>
-                    <tr>
-                      <th>Index</th>
-                      <th>Open</th>
-                      <th>High</th>
-                      <th>Low</th>
-                      <th>Close</th>
+          {/* history */}
+          <div className="w-10% desktop">
+          <h2 className="pb-2">10 Days</h2>
+            {apiData && apiData.length > 0 ? (
+            <div className = "mt-8 overflow-y-auto" style = {{ maxHeight: '200px' }}>
+              <table>
+                <thead>
+                  <tr className = "border-b">
+                    <th>Index</th>
+                    <th>Open</th>
+                    <th>High</th>
+                    <th>Low</th>
+                    <th>Close</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {apiData.map((item, index) => (
+                    <tr key={index}>
+                      <td className="with-border">{index}</td>
+                      <td className="with-border">{item.open}</td>
+                      <td className="with-border">{item.high}</td>
+                      <td className="with-border">{item.low}</td>
+                      <td>{item.close}</td>
                     </tr>
-                  </thead>
-                  <tbody>
-                    {apiData.map((item, index) => (
-                      <tr key={index}>
-                        <td>{index}</td>
-                        <td>{item.open}</td>
-                        <td>{item.high}</td>
-                        <td>{item.low}</td>
-                        <td>{item.close}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
+                  ))}
+                </tbody>
+              </table>
+            </div>
             ) : (
               <p>stock history</p>
             )}
           </div>
-          <div className="w-30%">
-              {apiData && apiData.length > 0 ? (
-              <div className = "mt-8 overflow-y-auto" style = {{ maxHeight: '200px' }}>
-                <h2>Data from API:</h2>
-                <table>
-                  <thead>
-                    <tr>
-                      <th>Index</th>
-                      <th>Open</th>
-                      <th>High</th>
-                      <th>Low</th>
-                      <th>Close</th>
+          <div className="w-10% desktop">
+          <h2 className="pb-2">1 Week</h2>
+            {apiData && apiData.length > 0 ? (
+            <div className = "mt-8 overflow-y-auto" style = {{ maxHeight: '200px' }}>
+              <table>
+                <thead>
+                  <tr className = "border-b">
+                    <th>Index</th>
+                    <th>Open</th>
+                    <th>High</th>
+                    <th>Low</th>
+                    <th>Close</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {apiData.map((item, index) => (
+                    <tr key={index}>
+                      <td className="with-border">{index}</td>
+                      <td className="with-border">{item.open}</td>
+                      <td className="with-border">{item.high}</td>
+                      <td className="with-border">{item.low}</td>
+                      <td>{item.close}</td>
                     </tr>
-                  </thead>
-                  <tbody>
-                    {apiData.map((item, index) => (
-                      <tr key={index}>
-                        <td>{index}</td>
-                        <td>{item.open}</td>
-                        <td>{item.high}</td>
-                        <td>{item.low}</td>
-                        <td>{item.close}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
+                  ))}
+                </tbody>
+              </table>
+            </div>
             ) : (
               <p>stock history</p>
             )}
           </div>
-          <div className="w-30%">
-            <p>statistic</p>
+          {/* predict */}
+          <div className="w-10% desktop">
+            <p className="border-b pb-2">Predict</p>
+            <div className="mt-8 overflow-y-auto" style={{ maxHeight: '200px' }}>
+              <table>
+              <thead>
+                <tr className = "border-b">
+                  <th>Day</th>
+                  <th>Predict price</th>
+                </tr>
+              </thead>
+              {predictData && predictData[0].map((data, index) => (
+                <tr key={index}>
+                  <td className = "with-border">{index}</td>
+                  <td>{data}</td>
+                </tr>
+              ))}
+              </table>
+            </div>
+          </div>
+          {/* statistics */}
+          <div className="w-10% desktop">
+            <p className="border-b pb-2">statistic</p>
+            <div className="mt-8 overflow-y-auto" style={{ maxHeight: '200px' }}>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>Best pdq AIC : </td>
+                    <td>[{errorData ? errorData.best_pdq_AIC : null}]</td>
+                  </tr>
+                  <tr>
+                    <td>Best pdq BIC : </td>
+                    <td>[{errorData ? errorData.best_pdq_MSE : null}]</td>
+                  </tr>
+                  <tr>
+                    <td>Percent ten days after : </td>
+                    <td>{predictData ? predictData[1].toFixed(2) : null} %</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </section>
